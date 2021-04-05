89cf81ceec7b23b097e0a28632f816bc37e425bb
Ilias Deros <IliasDeros@users.noreply.github.com>
2021-04-05 00:06:51 -0400

    Add a test for no collisions

--- test/ScreenCollision.test.ts
+++ test/ScreenCollision.test.ts
@@ -2,17 +2,28 @@ import { Sprite } from "pixi.js";
 import { ScreenCollision } from "../src/ScreenCollision";
 import { Bump } from "../vendor/Bump";
 
+const bullet = {} as Sprite;
+const letter = {} as Sprite;
+
 describe("ScreenCollision", () => {
   describe("#collidesBulletsLetters", () => {
-    it("returns one collision", () => {
-      const bullet = {} as Sprite;
-      const letter = {} as Sprite;
+    it("returns no collision", () => {
       const bumpMock = ({
-        hitTestRectangle: jest
-          .fn()
-          .mockImplementation(
-            (sprite, other) => sprite === bullet && other === letter
-          )
+        hitTestRectangle() {}
+      } as unknown) as Bump;
+
+      const unit = new ScreenCollision([bullet], [letter], {
+        bumpMock
+      });
+      const collisions = unit.collidesBulletsLetters();
+
+      expect(collisions).toEqual([]);
+    });
+
+    it("returns one collision", () => {
+      const bumpMock = ({
+        hitTestRectangle: (sprite: Sprite, other: Sprite) =>
+          sprite === bullet && other === letter
       } as unknown) as Bump;
 
       const unit = new ScreenCollision([bullet], [letter], {
